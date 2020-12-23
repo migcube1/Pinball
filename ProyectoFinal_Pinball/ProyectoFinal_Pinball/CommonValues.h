012d1a878ede6f7501ac9ef36b060f13d5042461
Ernesto2998 <neto.vel.arci2998@gmail.com>
2020-12-22 19:37:17 -0600

    Luces y canica

--- ProyectoFinal_Pinball/ProyectoFinal_Pinball/CommonValues.h
+++ ProyectoFinal_Pinball/ProyectoFinal_Pinball/CommonValues.h
@@ -2,6 +2,6 @@
 #define COMMONVALS
 #include "stb_image.h"
 
-const int MAX_POINT_LIGHTS = 3;
-const int MAX_SPOT_LIGHTS = 10;
+const int MAX_POINT_LIGHTS = 1;
+const int MAX_SPOT_LIGHTS = 4;
 #endif
